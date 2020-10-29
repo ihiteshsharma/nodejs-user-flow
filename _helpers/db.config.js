a0f533999f587e78633ec56eb466640879ec601a
Hitesh Sharma <hitesh9sharma@gmail.com>
2020-10-29 19:04:44 +0530

    Updated dbconfig to remove in-memory server and use db separately instead

--- _helpers/db.config.js
+++ _helpers/db.config.js
@@ -1,34 +1,40 @@
 //src/config/db.config.js
 
-//defines a mongodbserver in memory ready for use
-
 const { MongoClient } = require('mongodb');
-const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const COLLECTIONS = ['users'];
+// as we connect to MongoDB Atlas, ip and port have not been explicitly set
+// if we use a separate db server, define variables in dotenv and import here
+// uncomment the below two lines if connecting to a separate db server
+
+const DBConfig = {
+    // server_ip: process.env.DATABASE_IP,
+    // server_port: process.env.DATABASE_PORT,
+    db_clusterURL: process.env.DATABASE_URL,
+    db_name: process.env.DATABASE_NAME,
+    db_username: process.env.DATABASE_USER,
+    db_userpassword: process.env.DATABASE_PASSWORD
+};
+
+const uri = "mongodb+srv://"+DBConfig.db_username+":"+DBConfig.db_userpassword+"@"+DBConfig.db_clusterURL+"/"+DBConfig.db_name+"?";
+
+const COLLECTIONS = ['users']
 
 class DBManager {
     constructor(){
-        this.uri = null
-        this.db = null;
-        this.server = new MongoMemoryServer();
+        this.uri = uri;
         this.connection = null;
+        this.db = null;
     }
     async start(){
-        const url = await this.server.getUri();
-        this.uri = url;
-        this.connection = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-        this.db = this.connection.db(await this.server.getDbName());
+        this.connection = await new MongoClient.connect(this.uri, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+        this.db = this.connection.db(DBConfig.db_name);
     }
     stop(){
         this.connection.close();
-        return this.server.stop();
     }
-
     cleanup(){
         return Promise.all(COLLECTIONS.map(collection => this.db.collection(collection).deleteMany({})));
     }
-
 }
 
 
